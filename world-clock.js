df92a791b653cf548e2b0c16c104191cd1c2dda9
Blessing-bit-ui <yembeblessing27@gmail.com>
2025-03-22 09:38:15 -0700

    imported moment.js. created a function called  updatecurrentDate and setinterval to it

--- world-clock.js
+++ world-clock.js
@@ -1,1 +1,17 @@
+function updateCurrentDate() {
+  let losAngelesDateElement = document.querySelector("#los-angeles");
+  let losAngelesDate = moment.tz("America/Los_Angeles").format("LL");
+  let losAngelesTimeElement = document.querySelector("#los-angeles-time");
+  let losAngelesTime = moment().tz("America/Los_Angeles").format("h:mm:ss");
+  losAngelesDateElement.innerHTML = losAngelesDate;
+  losAngelesTimeElement.innerHTML = losAngelesTime;
 
+  let parisDateElement = document.querySelector("#paris");
+  let parisDate = moment.tz("Europe/Paris").format("LL");
+  let parisTimeElement = document.querySelector("#paris-time");
+  let parisTime = moment().tz("Europe/Paris").format("h:mm:ss");
+  parisDateElement.innerHTML = parisDate;
+  parisTimeElement.innerHTML = parisTime;
+}
+updateCurrentDate();
+setInterval(updateCurrentDate, 1000);
